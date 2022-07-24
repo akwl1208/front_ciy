1118452c78702762aa562a7bbbc1065e987b6407
akwl1208 <104331415+akwl1208@users.noreply.github.com>
2022-07-24 21:48:58 +0900

    220724 과제: box-menu_네이버_js_jquery

--- jscript_naver/box-menu_event.js
+++ jscript_naver/box-menu_event.js
@@ -2,7 +2,13 @@
 $(function () {
 	let boxMenu = '.menu-con .box-menu ';
 	$(boxMenu+'.btn-more').click(function () {
-		// 더보기 버튼을 클릭하면 fold 클래스를 추가하고 빼고 하는 이벤트
+		/* 더보기 버튼을 클릭하면 접기 버튼이 나타나고, 전체 서비스 보기 버튼과
+			서비스 박스가 나타나는 이벤트.
+			접기 버튼을 클릭하면 더보기 버튼이 나타나고 전체 서비스 보기와 서비스 박스가 사라짐*/
 		$(this).toggleClass('fold');
+		$(boxMenu+'.btn-area').toggleClass('fold');
+		$('.menu-con .service-con').toggleClass('fold');
 	})
+
+
 })
